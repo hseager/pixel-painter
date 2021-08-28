212a746e83faf0f2f7b2297962b1032c6efbbcc1
hseager <harvey.seager@googlemail.com>
2021-08-28 16:08:09 +0100

    Updates

---
+++ precache-manifest.3c30368ec0cd7ceb543b60a05cff2bd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b886b1d313b0db2a523bd06491d0b0a5",
+    "url": "/pixel-painter/index.html"
+  },
+  {
+    "revision": "04840d177318b15297cb",
+    "url": "/pixel-painter/static/css/main.73f8a4f0.chunk.css"
+  },
+  {
+    "revision": "f7b050778db7c04f0068",
+    "url": "/pixel-painter/static/js/2.6a1b7a09.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pixel-painter/static/js/2.6a1b7a09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04840d177318b15297cb",
+    "url": "/pixel-painter/static/js/main.9632918a.chunk.js"
+  },
+  {
+    "revision": "83e0b7a1aaa43d11ed7c",
+    "url": "/pixel-painter/static/js/runtime-main.acb7da38.js"
+  }
+]);
